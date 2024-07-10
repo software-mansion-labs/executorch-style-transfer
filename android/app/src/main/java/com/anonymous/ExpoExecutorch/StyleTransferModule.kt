7f37fe5294712d1d2b9ade2fb5872549eba0efb0
chmjkb <jakubchmura1607@gmail.com>
2024-07-10 14:48:15 +0200

    Rename Android variables to match iOS, adjust try-catch scope

--- android/app/src/main/java/com/anonymous/ExpoExecutorch/StyleTransferModule.kt
+++ android/app/src/main/java/com/anonymous/ExpoExecutorch/StyleTransferModule.kt
@@ -9,59 +9,68 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import org.pytorch.executorch.EValue
 import org.pytorch.executorch.Module
+import org.pytorch.executorch.Tensor
 import java.io.File
 import java.io.FileOutputStream
 
 class StyleTransferModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
     override fun getName() = "StyleTransferModule"
-    private val modelResourceIds: Map<String, Int> = mapOf(
+    private val modelNameToResourceId: Map<String, Int> = mapOf(
         "mosaic" to R.raw.mosaic_xnnpack_640_fp32,
         "candy" to R.raw.candy_xnnpack_640_fp32,
         "rain_princess" to R.raw.rain_princess_xnnpack_640_fp32,
         "udnie" to R.raw.udnie_xnnpack_640_fp32
     )
-    private val modules: MutableMap<String, Module> = mutableMapOf()
+    private val models: MutableMap<String, Module> = mutableMapOf()
 
     @ReactMethod
     fun initModules(promise: Promise) {
         try {
-            modelResourceIds.forEach { (modelName, resourceId) ->
-                modules[modelName] = loadModuleFromRawResource(resourceId)
+            modelNameToResourceId.forEach { (modelName, resourceId) ->
+                models[modelName] = loadModuleFromRawResource(resourceId)
             }
             promise.resolve("All models loaded successfully")
         } catch (e: Exception) {
-            promise.reject("Cannot load all models", e)
+            promise.reject("InitModulesError", e)
         }
     }
 
     @ReactMethod
-    fun applyStyleTransfer(styleName: String, imageUri: String, promise: Promise) {
-        try {
-            // load an image (bitmap) from URI
-            val uri = Uri.parse(imageUri)
-            val bitmapInputStream = reactApplicationContext.contentResolver.openInputStream(uri)!!
-            val rawBitmap = BitmapFactory.decodeStream(bitmapInputStream)
-            bitmapInputStream.close()
-
-            // rotate if needed, resize and  make it a tensor
-            val rotatedBitmap = BitmapUtils.handleBitmapOrientation(uri, reactApplicationContext.contentResolver, rawBitmap)
-            val inputBitmap = Bitmap.createScaledBitmap(
-                rotatedBitmap,
-                640, 640, true
-            )
-            val inputTensor = TensorUtils.bitmapToFloat32Tensor(inputBitmap)
-
-            // run the model
-            val outputTensor = modules[styleName]!!.forward(EValue.from(inputTensor))[0].toTensor()
-
-            // convert the output back to a bitmap and save the result
-            val outputBitmap = TensorUtils.float32TensorToBitmap(outputTensor)
-            val outputUri = BitmapUtils.saveToTempFile(outputBitmap, styleName)
-
-            promise.resolve(outputUri.toString())
-        } catch (e: Exception) {
-            promise.reject("Error", e)
+    fun applyStyleTransfer(moduleName: String, imageUri: String, promise: Promise) {
+        val module = models[moduleName]
+        if (module == null) {
+            val errorMessage = "Module $moduleName was not initialized!"
+            promise.reject("ModelNotInitializedError", Error(errorMessage))
+            return
         }
+        // load an image (bitmap) from URI
+        val uri = Uri.parse(imageUri)
+        val bitmapInputStream = reactApplicationContext.contentResolver.openInputStream(uri)!!
+        val rawBitmap = BitmapFactory.decodeStream(bitmapInputStream)
+        bitmapInputStream.close()
+
+        // rotate if needed, resize and  make it a tensor
+        val rotatedBitmap = BitmapUtils.handleBitmapOrientation(uri, reactApplicationContext.contentResolver, rawBitmap)
+        val inputBitmap = Bitmap.createScaledBitmap(
+            rotatedBitmap,
+            640, 640, true
+        )
+        val inputTensor = TensorUtils.bitmapToFloat32Tensor(inputBitmap)
+
+        // wrap the tensor with EValue, pass it to the .forward() method of the module
+        lateinit var outputTensor : Tensor
+        try {
+            module.forward(EValue.from(inputTensor))[0].toTensor()
+                .also { outputTensor = it }
+        } catch(e: Exception) {
+            promise.reject("InferenceError", e)
+        }
+
+        // convert the output back to a bitmap and save the result
+        val outputBitmap = TensorUtils.float32TensorToBitmap(outputTensor)
+        val outputUri = BitmapUtils.saveToTempFile(outputBitmap, moduleName)
+        // return the URI to the saved file
+        promise.resolve(outputUri.toString())
     }
 
     private fun loadModuleFromRawResource(resourceId: Int): Module {
